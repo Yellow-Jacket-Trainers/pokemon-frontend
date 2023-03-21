66229a78b41cf1053e255bb4f7cbacf92c1bb474
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-03-21 11:09:12 -0700

    carousel/put

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ import {
 } from "react-router-dom";
 
 import Pokemon from './Components/PokemonForm';
-// import Home from './Home';
 import Header from './Header';
 import Footer from './Footer'
-// import About from './About';
 
 class App extends React.Component{
   constructor(props) {
@@ -28,13 +26,30 @@ class App extends React.Component{
 
   getPokeData = async (e) => {
     e.preventDefault();
-    let pokeData = await axios.get(`${process.env.REACT_APP_SERVER}/pokemon?name=${this.state.pokeName}`)
-    console.log(pokeData.data)
-    this.setState({
-      pokeData:pokeData.data,
-    }, console.log(this.state.pokeData))
-    //console.log(this.state.movieData)
+    try {
+      let pokeData = await axios.get(`${process.env.REACT_APP_SERVER}/pokemon?name=${this.state.pokeName}`);
+      console.log(pokeData.data);
+      this.setState({
+        pokeData: pokeData.data,
+      }, () => console.log(this.state.pokeData));
+    } catch (error) {
+      console.error(error);
+    }
   }
+  
+  updatePokemon = async (e) => {
+    e.preventDefault();
+    try {
+      let updatedPokemon = await axios.put(`${process.env.REACT_APP_SERVER}/pokemon/${this.state.pokeData._id}`, this.state.pokeData);
+      console.log(updatedPokemon.data);
+      this.setState({
+        pokeData: updatedPokemon.data,
+      }, () => console.log(this.state.pokeData));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
 
   DeletePokemon = async (e) => {
     e.preventDefault();
@@ -47,46 +62,52 @@ class App extends React.Component{
     }, console.log(this.state.pokeName));
   };
 
-  
-
-
-
   render(){
-    return (
+    let pokemonItems = [];
+    if (this.state.pokeData.length) {
+      pokemonItems = this.state.pokeData.map(pokeData => (
+        <Carousel.Item key={pokeData._id}>
+          <img
+            className="d-block w-100"
+            // src="book-bg.jpg"
+            alt="Pokemon"
+          />
+    
+          <Carousel.Caption>
+            <h3>{pokeData.title}</h3>
+            <p>{pokeData.description}</p>
+            <p>{pokeData.status}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ));
+    }
+
+    return (    
       <>
         <Router>
           <Header />
           <Routes>
-
-              <Route
+            <Route
               exact path="/"
               element={<Pokemon 
                 getPokeData={this.getPokeData}
                 handlePokeInput={this.handlePokeInput}/>}>
             </Route>
-
             {/* <Route
               path="/about"
               element={<About />}>
               </Route> */}
-
               {/* <Route
               path="/home"
               element={<Home />}>
               </Route> */}
-
-            </Routes>
-            
-            <Footer />
+          </Routes>
+          <Footer />
         </Router>
+        <Carousel>{pokemonItems}</Carousel>
       </>
-
-
     )
-
-
   }
-
 }
 
 export default App;
